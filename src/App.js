989e95cd6b4fb523b7d1eb352427a314d408aa18
Susan Zhang <susan.zhang901@gmail.com>
2023-03-01 10:35:16 -0600

    Changed how the API is called

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
 
+
 function App() {
   const [dog, setDog] = useState({
     randomImage: "https://images.dog.ceo/breeds/sheepdog-shetland/n02105855_9903.jpg"
   })
-  const [allDogs, setAllDogs] = useState([])
+  const [allDogs, setAllDogs] = useState("");
+  const [count, setCount] = React.useState(0);
+
   useEffect(() => {
-        fetch("https://dog.ceo/api/breeds/image/random/3")
-            .then(res => res.json())
-            .then(data => setAllDogs(data.message))
-    }, [])
+    const fetchDog = async () => {
+      const result = await fetch("https://dog.ceo/api/breeds/image/random")
+      result.json().then(json => {
+        console.log(json);
+        setAllDogs(json.message)
+      })
+    }
+    fetchDog();
+  }, [count])
+
   function getDog() {
-    const rand = Math.floor(Math.random() * allDogs.length);
-    console.log(`Rand here is ${rand}`)
-    const url = allDogs[rand]
+    setCount(prevCount => prevCount+1)
+    const url = allDogs
     setDog({randomImage: url});
     console.log(`Dog here is: ` + dog.randomImage);
-    console.log(`Dog here is: ` + url);
-    console.log(`allDogs.length is: ` + allDogs.length);
   }
+
+
   return (
     <div className="App">
       <div className="blur" style={{top: '-18%', right: '0'}}></div>
